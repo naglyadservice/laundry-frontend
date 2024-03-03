06354c40ffbcc7c39edd43569564ab440ed888cb
MrInverted <akson.dyuk@gmail.com>
2024-03-03 09:12:08 +0200

    slug

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Html from "./Html";
 import Spinner from "./Spinner";
 
 
+const pathnameArrFiltered = window.location.pathname
+  .split("/")
+  .filter((item) => item !== "");
+
+const slug = pathnameArrFiltered[pathnameArrFiltered.length - 1];
+
+
+
 function App() {
   const [data, setData] = React.useState({ isLoading: true });
   const [status, setStatus] = React.useState({
@@ -12,11 +20,11 @@ function App() {
   })
 
   React.useEffect(() => {
-    fetch("https://wash.npc.com.ua/api/waher/toxa-local/info")
+    fetch(`https://wash.npc.com.ua/api/waher/${slug}/info`)
       .then(data => data.json())
       .then(data => setData({ ...data, isLoading: false }));
 
-    fetch("https://wash.npc.com.ua/api/waher/toxa-local/pay")
+    fetch(`https://wash.npc.com.ua/api/waher/${slug}/pay`)
       .then(data => data.json())
       .then(data => setData(prev => ({ ...prev, ...data })));
   }, [])
